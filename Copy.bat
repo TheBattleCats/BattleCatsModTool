1ad7989be544f914af06b82845cd7499912bd8e4
TheBattleCats <44732628+TheBattleCats@users.noreply.github.com>
2018-11-03 14:45:34 -0400

    Add files via upload

---
+++ Copy.bat
@@ -0,0 +1,4 @@
+@echo off
+copy Decompiled_APK\assets\DataLocal.list Decryptor && copy Decompiled_APK\assets\DataLocal.pack Decryptor && Decryptor\decrypt.bat
+pause
+
---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+ImportantScripts\autochecksum742.bat
+apktool build --output BuiltAPK\battlecats.apk Decompiled_APK
